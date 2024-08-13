7bc57c0451e35317af6cdcaeb578fd9eb7dfac96
elidhan <kurtbaronagliam@gmail.com>
2024-08-13 12:18:40 +0800

    -Tweaked Desert Rifle reload animation -Cajun seasoning

--- src/main/java/net/elidhan/anim_guns/client/MuzzleFlashRenderType.java
+++ src/main/java/net/elidhan/anim_guns/client/MuzzleFlashRenderType.java
@@ -1,10 +1,7 @@
 package net.elidhan.anim_guns.client;
 
 import net.elidhan.anim_guns.AnimatedGuns;
-import net.minecraft.client.render.RenderLayer;
-import net.minecraft.client.render.RenderPhase;
-import net.minecraft.client.render.VertexFormat;
-import net.minecraft.client.render.VertexFormats;
+import net.minecraft.client.render.*;
 import net.minecraft.util.Identifier;
 
 public class MuzzleFlashRenderType extends RenderLayer
@@ -14,15 +11,10 @@ public class MuzzleFlashRenderType extends RenderLayer
         super(name, vertexFormat, drawMode, expectedBufferSize, hasCrumbling, translucent, startAction, endAction);
     }
 
-    private static final RenderLayer MUZZLE_FLASH = of(AnimatedGuns.MOD_ID+":muzzle_flash", VertexFormats.POSITION_COLOR_TEXTURE_LIGHT, VertexFormat.DrawMode.QUADS, 256, true, false, MultiPhaseParameters.builder().program(RenderPhase.EYES_PROGRAM).texture(new RenderPhase.Texture(new Identifier(AnimatedGuns.MOD_ID, "textures/misc/muzzleflash.png"), false, false)).transparency(TRANSLUCENT_TRANSPARENCY).cull(RenderPhase.DISABLE_CULLING).build(false));
-    private static final RenderLayer MUZZLE_SMOKE = of(AnimatedGuns.MOD_ID+":muzzle_smoke", VertexFormats.POSITION_COLOR_TEXTURE, VertexFormat.DrawMode.QUADS, 256, true, false, MultiPhaseParameters.builder().program(RenderPhase.POSITION_COLOR_TEXTURE_PROGRAM).texture(new RenderPhase.Texture(new Identifier(AnimatedGuns.MOD_ID, "textures/misc/muzzlesmoke.png"), false, false)).transparency(TRANSLUCENT_TRANSPARENCY).cull(RenderPhase.DISABLE_CULLING).build(false));
+    private static final RenderLayer MUZZLE_FLASH = of("anim_guns:muzzle_flash", VertexFormats.POSITION_COLOR_TEXTURE_LIGHT, VertexFormat.DrawMode.QUADS, 256, false, true, MultiPhaseParameters.builder().program(RenderPhase.POSITION_COLOR_TEXTURE_LIGHTMAP_PROGRAM).texture(new RenderPhase.Texture(new Identifier(AnimatedGuns.MOD_ID, "textures/misc/muzzleflash.png"), false,false)).transparency(RenderPhase.TRANSLUCENT_TRANSPARENCY).build(false));
 
     public static RenderLayer getMuzzleFlash()
     {
         return MUZZLE_FLASH;
-    }
-    public static RenderLayer getMuzzleSmoke()
-    {
-        return MUZZLE_SMOKE;
     }
 }
