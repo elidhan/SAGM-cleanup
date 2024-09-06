9ca7039610d6e9f1865779097963fad8c14947a9
elidhan <84767537+elidhan@users.noreply.github.com>
2024-09-06 21:45:47 +0800

    -Updated Ammo Counter logic and textures

--- src/main/java/net/elidhan/anim_guns/event/client/HudRenderHandler.java
+++ src/main/java/net/elidhan/anim_guns/event/client/HudRenderHandler.java
@@ -8,8 +8,6 @@ import net.minecraft.client.gui.DrawContext;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
-import java.awt.*;
-
 public class HudRenderHandler implements HudRenderCallback
 {
     private static final Identifier AMMO_ICONS = new Identifier(AnimatedGuns.MOD_ID, "textures/gui/ammo_icons.png");
@@ -18,22 +16,40 @@ public class HudRenderHandler implements HudRenderCallback
     public void onHudRender(DrawContext drawContext, float tickDelta)
     {
         MinecraftClient client = MinecraftClient.getInstance();
-        if (!(client.player.getMainHandStack().getItem() instanceof GunItem)) return;
+        if (!(client.player.getMainHandStack().getItem() instanceof GunItem gun)) return;
 
         int ammoCount = client.player.getMainHandStack().getOrCreateNbt().getInt("ammo");
-        int maxAmmo = ((GunItem)client.player.getMainHandStack().getItem()).getMagSize();
+        int maxAmmo = gun.getMagSize();
 
         int scaledWidth = drawContext.getScaledWindowWidth();
         int scaledHeight = drawContext.getScaledWindowHeight();
 
-        Text text = Text.translatable(ammoCount + "/" + maxAmmo);
+        Text text = Text.translatable(String.valueOf(ammoCount));
+        if (ammoCount < 10) text = Text.translatable("0"+ammoCount);
 
-        drawContext.drawTextWithShadow(client.textRenderer, text, scaledWidth/2 + 93,scaledHeight - 22  , 16777215);
-        if(ammoCount < 20)
+        drawContext.drawTextWithShadow(client.textRenderer, text, scaledWidth - client.textRenderer.getWidth(text) - 12,scaledHeight - 32  , 16777215);
+        if(maxAmmo < 20)
         {
+            for (int i = 0; i < maxAmmo; i++)
+            {
+                drawContext.drawTexture(AMMO_ICONS, scaledWidth - (i*6 - i) - client.textRenderer.getWidth(text) - 20, scaledHeight - 36, 2, 16, 7, 16);
+            }
             for (int i = 0; i < ammoCount; i++)
             {
-                drawContext.drawTexture(AMMO_ICONS, scaledWidth/2 + 95 + (i*6)-(i), scaledHeight - 18, 0, 0, 8, 16);
+                drawContext.drawTexture(AMMO_ICONS, scaledWidth - (i*6 - i) - client.textRenderer.getWidth(text) - 20, scaledHeight - 36, 2, 0, 7, 16);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < maxAmmo; i++)
+            {
+                int j = i / (maxAmmo / 2);
+                drawContext.drawTexture(AMMO_ICONS, scaledWidth - (i*3 - i ) - client.textRenderer.getWidth(text) - 20 + (j * maxAmmo), scaledHeight - (28 + (8 * j)), 11, 8, 3, 8);
+            }
+            for (int i = 0; i < ammoCount; i++)
+            {
+                int j = i / (maxAmmo / 2);
+                drawContext.drawTexture(AMMO_ICONS, scaledWidth - (i*3 - i ) - client.textRenderer.getWidth(text) - 20 + (j * maxAmmo), scaledHeight - (28 + (8 * j)), 11, 0, 3, 8);
             }
         }
     }
